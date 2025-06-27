283056424b8890f6887568e7a65304c41e17141e
Richard Kaye <rdkaye444@gmail.com>
2025-06-27 11:05:41 -0400

    now querying chroma

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,6 +1,7 @@
 from schema.document import Document
 from rag.embedding import Embedder
 from sentence_transformers import CrossEncoder
+from rag.vectorstore import VectorStore
 
 class Retriever:
     def __init__(self, 
@@ -8,9 +9,11 @@ class Retriever:
                  ranker_model_name:str = 'cross-encoder/ms-marco-MiniLM-L-6-v2'):
         self.embedder = Embedder(embedder_model_name)
         self.document_ranker = CrossEncoder(ranker_model_name)
+        self.vector_store = VectorStore()
 
-    def retrieve(self, query: str)-> list[Document]:
-        pass
+    def retrieve(self, query: str, n_results: int = 10)-> list[Document]:
+        documents = self.vector_store.query(query, n_results)
+        return self.reorder_documents(documents, query)
 
     def reorder_documents(self, documents: list[Document], query: str) -> list[Document]:
         """
