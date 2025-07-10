912b3417173bc18090e204785f0ee543bb8224ad
Richard Kaye <rdkaye444@gmail.com>
2025-07-10 06:09:35 -0400

    Refactor generation tests to use == instead of != when determining test result

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -9,6 +9,16 @@ def test_generate_platypus_strict_should_judge_true(pipeline_factory):
     response = pipeline.run("Why is a platypus so weird?")
     judge = Judge()
     result = judge.judge(response, retriever.last_documents)
-    if result != JudgeResult.TRUE:
+    if result == JudgeResult.FALSE:
+        explanation = judge.explain(response, retriever.last_documents)
+        pytest.fail(f"Judgment was {result.name}. Explanation:\n{explanation}")
+
+def test_generate_platypus_loose_should_judge_false(pipeline_factory):
+    gen_config = GeneratorConfig(mode="loose")
+    pipeline, retriever, _  = pipeline_factory(gen_config)
+    response = pipeline.run("Why is a platypus so weird?")
+    judge = Judge()
+    result = judge.judge(response, retriever.last_documents)
+    if result == JudgeResult.TRUE:
         explanation = judge.explain(response, retriever.last_documents)
         pytest.fail(f"Judgment was {result.name}. Explanation:\n{explanation}")
